be6d9d32848439ff67957c85008255ce313b4b32
Liam's Laptop <66572834+jlgingrich@users.noreply.github.com>
2021-04-30 17:20:45 -0400

    Login Start

--- Config.php
+++ Config.php
@@ -1,10 +1,10 @@
 <?php
-	echo "Hello!"
-	$db = mysqli_connect(HOSTNAME,USERNAME,PASSWORD,DATABASE);
-
-	// Check connection
-	if (mysqli_connect_errno()) {
-		echo "Failed to connect to MySQL: " . mysqli_connect_error();
-		exit();
-	}
+// Database connection is stored as DigitalOcean environment variables
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(HOSTNAME,USERNAME,PASSWORD,DATABASE);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
 ?>
